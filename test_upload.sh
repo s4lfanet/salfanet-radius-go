e687b94aff196e980c6cba94932e108f9de6db72
s4lfanet <wardian370@gmail.com>
2026-08-09 02:24:48 +0700

    chore: remove remaining test scripts (test_genieacs.sh, test_genieacs_auth.sh, test_upload.sh)

--- test_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "=== Login response ==="
-curl -s -X POST http://127.0.0.1:8080/api/auth/login -H 'Content-Type: application/json' -d '{"username":"superadmin","password":"admin123"}'
-echo
-echo "=== Login with email ==="
-curl -s -X POST http://127.0.0.1:8080/api/auth/login -H 'Content-Type: application/json' -d '{"email":"admin@example.com","password":"admin123"}'
-echo
